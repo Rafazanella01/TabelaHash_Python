e84865a97572af9461cf6985b9b8b695ce44d80e
Rafazanella01 <rafazanella2004@gmail.com>
2024-04-15 16:02:29 -0300

    Quase pronto

--- TabelaHash.py
+++ TabelaHash.py
@@ -1,86 +1,117 @@
-import random
+import random  #Importa o módulo random para gerar números aleatórios
 
 class TabelaHash:
     def __init__(self, tamanho):
+        #Inicializa a classe TabelaHash com o tamanho especificado
         self.tamanho = tamanho
-        self.tabela = [[] for _ in range(tamanho)]
 
-    def hash(self, chave):
-        return chave % self.tamanho
+        #Cria a tabela hash como uma lista de listas vazias, uma lista para cada "slot" na tabela
+        self.tabela = [[] for _ in range(tamanho)] 
 
-    def inserir(self, chave, valor):
-        indice = self.hash(chave)
-        for item in self.tabela[indice]:
-            if item[0] == chave:
-                # Se a chave já existir, atualize o valor
-                item[1] = valor
-                return
-        # Se a chave não existir, adicione um novo par chave-valor
-        self.tabela[indice].append([chave, valor])
+    def hash(self, matricula): #Função hash que mapeia uma matrícula para um índice na tabela hash
+        
+        parteNumerica = int(matricula[1:]) #Extrai a parte numérica da matrícula e calcula o resto da divisão matric % tam
+        return parteNumerica % self.tamanho
 
-    def buscar(self, chave):
-        indice = self.hash(chave)
-        for item in self.tabela[indice]:
-            if item[0] == chave:
-                return item[1]
-        return None
+    def inserir(self, matricula, valor): #Insere um par (matrícula, valor) na tabela hash
+       
+        indice = self.hash(matricula) #Calcula o índice usando a função hash
+        for item in self.tabela[indice]:  #Percorre a lista correspondente ao índice na tabela hash
+            if item[0] == matricula:  #Verifica se a matrícula já está na lista
+                item[1] = valor #Se encontrou a matrícula, atualiza o valor associado
+                return  #Sai da função após atualizar o valor
+            
+        #Se a matrícula não foi encontrada na lista, adiciona um novo par (matrícula, valor)
+        self.tabela[indice].append([matricula, valor])
+
+    def buscar(self, matricula): #Busca o valor associado a uma matrícula na tabela hash
+
+        indice = self.hash(matricula)  #Calcula o índice usando a função hash
+        for item in self.tabela[indice]: #Percorre a lista correspondente ao índice na tabela hash
+            if item[0] == matricula:  #Verifica se a matrícula está na lista
+                return item[1]  #Se encontrou a matrícula, retorna o valor associado
+       
+        return None  #Se a matrícula não foi encontrada, retorna None
     
-    def excluir(self, chave):
-        indice = self.hash(chave)
-        lista = self.tabela[indice]
-        for item in lista:
-            if item[0] == chave:
-                lista.remove(item)
-                return True
-        return False
+    def buscarSequencial(self, matricula): #Busca sequencialmente o valor associado a uma matrícula na tabela hash
+
+        for lista in self.tabela:   #Percorre todas as listas na tabela hash
+            for item in lista:  #Percorre todos os itens em cada lista
+                if item[0] == matricula:  #Verifica se a matrícula está presente
+                    return item[1] #Se encontrou a matrícula, retorna o valor associado
+                
+       
+        return None #Se a matrícula não foi encontrada, retorna Nulo
+    
+    def excluir(self, matricula): #Exclui um par (matrícula, valor) da tabela hash
+       
+        indice = self.hash(matricula) #Calcula o índice usando a função hash
+        lista = self.tabela[indice] #Obtém a lista correspondente ao índice na tabela hash
+        for item in lista: #Percorre a lista
+            if item[0] == matricula:   #Verifica se a matrícula está presente
+                lista.remove(item) #Se encontrou a matrícula, remove o par da lista
+                return True  #Retorna True para indicar que a exclusão foi bem-sucedida
+      
+        return False #Se a matrícula não foi encontrada, retorna False
     
     def ordenar(self):
-        for lista in self.tabela:
-            lista.sort()
-        print("Tabela ordenada com sucesso.")
+        for lista in self.tabela: #Ordena todas as listas na tabela hash (não é uma operação comum em tabelas hash)
+            lista.sort()  #Ordena a lista atual
+        print("Tabela ordenada com sucesso.") #Exibe uma mensagem indicando que a ordenação foi concluída
 
     @staticmethod   
-    def criarBaseDados(tamanho):
+    def criarBaseDados(tamanho): #Cria uma base de dados aleatória com matrículas e valores associados
         base_dados = []
         for _ in range(tamanho):
-            chave = random.randint(1, 999)
+            #Gera uma matrícula no formato 'mxxxxxx' e um valor aleatório entre 1 e 999
+            matricula = f'm{random.randint(0, 999999):06d}'
             valor = random.randint(1, 999)
-            base_dados.append((chave, valor))
+          
+            base_dados.append((matricula, valor)) #Adiciona o par (matrícula, valor) à base de dados
         return base_dados
     
-    def visualizar(self):
+    def visualizar(self): #Exibe a tabela hash na tela, mostrando cada índice e sua lista correspondente
         for i in range(len(self.tabela)):
             print(f"Índice {i}: {self.tabela[i]}")
 
-    def buscarPorChave(self):
-        chave_busca = int(input("Digite a chave que deseja buscar: "))
-        valor_encontrado = self.buscar(chave_busca)
+    def buscarPorMatricula(self): #Solicita ao usuário uma matrícula e busca o valor correspondente na tabela hash
+        
+        matricula_busca = input("Digite a matrícula que deseja buscar: ")
+        valor_encontrado = self.buscar(matricula_busca)
         if valor_encontrado is not None:
-            print(f"Valor encontrado para a chave {chave_busca}: {valor_encontrado}")
+            #Se encontrou a matrícula, exibe o valor associado
+            print(f"Valor encontrado para a matrícula {matricula_busca}: {valor_encontrado}")
         else:
-            print(f"Nenhum valor encontrado para a chave {chave_busca}")
+            #Se a matrícula não foi encontrada, exibe uma mensagem de erro
+            print(f"Nenhum valor encontrado para a matrícula {matricula_busca}")
 
-    def imprimirTodosValores(self):
+    def imprimirTodosValores(self): #Exibe todos os pares (matrícula, valor) presentes na tabela hash formatados
+       
         for lista in self.tabela:
-            if lista:
-                for chave, valor in lista:
-                    print(f"Chave: {chave}, Valor: {valor}")
+            if lista:  #Verifica se a lista não está vazia
+                for matricula, valor in lista:
+                    print(f"Matrícula: {matricula}, Valor: {valor}") #Exibe a matrícula e o valor associado
 
-    def preencherComBaseDados(self):
+    def preencherComBaseDados(self): #Preenche a tabela hash com uma base de dados aleatória
+        
         base_dados = self.criarBaseDados(self.tamanho)
-        for chave, valor in base_dados:
-            self.inserir(chave, valor)
-        print("Tabela preenchida com sucesso.")
+        for matricula, valor in base_dados:
+            self.inserir(matricula, valor) #Insere cada par (matrícula, valor) na tabela hash
+        print("Tabela preenchida com sucesso.")  #Exibe uma mensagem indicando que o preenchimento foi concluído
 
-    def excluirPorChave(self):
-        chave_exclusao = int(input("Digite a chave que deseja excluir: "))
-        if self.excluir(chave_exclusao):
-            print(f"Chave {chave_exclusao} excluída com sucesso.")
+    def excluirPorMatricula(self):
+        #Solicita ao usuário uma matrícula e exclui o par correspondente da tabela hash
+        matricula_exclusao = input("Digite a matrícula que deseja excluir: ")
+        if self.excluir(matricula_exclusao): #Se a exclusão foi bem-sucedida, exibe uma mensagem de sucesso
+            print(f"Matrícula {matricula_exclusao} excluída com sucesso.")
         else:
-            print(f"Chave {chave_exclusao} não encontrada na tabela.")
+            #Se a matrícula não foi encontrada, exibe uma mensagem de erro
+            print(f"Matrícula {matricula_exclusao} não encontrada na tabela.")
 
     def inserirNovoValor(self):
-        chave = int(input("Digite a chave que deseja inserir: "))
-        valor = input("Digite o valor que deseja associar à chave: ")
-        self.inserir(chave, valor)
-        print("Valor inserido com sucesso.")
+        #Solicita ao usuário uma nova matrícula e um novo valor, e insere na tabela hash
+        matricula = input("Digite a matrícula que deseja inserir: ")
+        valor = input("Digite o valor que deseja associar à matrícula: ")
+        #Insere o par (matrícula, valor) na tabela hash
+        self.inserir(matricula, valor)
+        print("Valor inserido com sucesso.")  #Exibe uma mensagem indicando que a inserção foi concluída
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import timeit
 from TabelaHash import TabelaHash
 
 # Criar a tabela hash
@@ -6,35 +7,47 @@ tabela = TabelaHash(tamanho_tabela)
 
 # Menu
 while True:
-    print("\nMenu:")
-    print("1. Buscar valor por chave")
-    print("2. Imprimir todos os valores")
-    print("3. Preencher tabela com base de dados")
-    print("4. Ordenar tabela")
-    print("5. Excluir valor por chave")
-    print("6. Inserir novo valor")
-    print("7. Visualizar toda tabela")
-    print("8. Sair")
+    try: #Bloco que será executado enquanto não ocorrer nenhuma exceção, se ocorrer algum problema, como um valor inváido, entrará no bloco except.
+        print("\nMenu:")
+        print("1. Buscar valor por matrícula")
+        print("2. Imprimir todos os valores")
+        print("3. Preencher tabela com base de dados")
+        #print("4. Ordenar tabela")
+        print("4. Excluir valor por matrícula")
+        print("5. Inserir novo valor")
+        print("6. Visualizar toda tabela")
+        print("7. Comparar busca sequencial e busca com hash")
+        print("8. Sair")
 
-    opcao = input("Escolha uma opção: ")
+        opcao = input("Escolha uma opção: ")
 
-    if opcao == "1":
-        tabela.buscarPorChave()
-    elif opcao == "2":
-        tabela.imprimirTodosValores()
-    elif opcao == "3":
-        tabela.preencherComBaseDados()
-    elif opcao == "4":
-        tabela.ordenar()
-    elif opcao == "5":
-        tabela.excluirPorChave()
-    elif opcao == "6":
-        tabela.inserirNovoValor()
-    elif opcao == "7":
-        tabela.visualizar()
-    elif opcao == "8":
-        print("Saindo...")
-        break
-    else:
-        print("Opção inválida. Por favor, escolha uma opção válida.")
+        if opcao == "1":
+            tabela.buscarPorMatricula()
+        elif opcao == "2":
+            tabela.imprimirTodosValores()
+        elif opcao == "3":
+            tabela.preencherComBaseDados()
+        #elif opcao == "4":
+            #tabela.ordenar()
+        elif opcao == "4":
+            tabela.excluirPorMatricula()
+        elif opcao == "5":
+            tabela.inserirNovoValor()
+        elif opcao == "6":
+            tabela.visualizar()
+        elif opcao == "7":
+            matriculaHash,  matriculaSequencial = input("Digite a matricula para ser comparada: ").split()
+            
+            tempoHash = timeit.timeit(lambda: tabela.buscar(matriculaHash), number=10000)
+            tempoSequencial = timeit.timeit(lambda: tabela.buscarSequencial(matriculaSequencial), number=10000)
 
+            print(f"Tempo médio de busca hash: {round(tempoHash, 5)} segundos")
+            print(f"Tempo médio de busca sequencial: {round(tempoSequencial, 5)} segundos")
+        
+        elif opcao == "8":
+            print("Saindo...")
+            break
+        else:
+            print("Opção inválida. Por favor, escolha uma opção válida.")
+    except Exception as e: #Esse é o bloco da exceção, ele irá imprimir uma mensagem com qual erro ocorreu na execução e voltará novamente para o bloco try.
+        print(f"Ocorreu um erro: {e}")
